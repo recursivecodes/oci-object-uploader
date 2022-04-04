2f72b64dae337734c3fe5b5c9f412af93c145ae3
recursivecodes <toddraymondsharp@gmail.com>
2022-04-04 13:20:22 -0400

    initial commit

--- src/main/java/codes/recursive/Application.java
+++ src/main/java/codes/recursive/Application.java
@@ -1,7 +1,29 @@
 package codes.recursive;
 
+import com.oracle.bmc.objectstorage.model.Bucket;
+import com.oracle.bmc.objectstorage.model.CreatePreauthenticatedRequestDetails;
+import com.oracle.bmc.objectstorage.model.PreauthenticatedRequest;
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
+import com.oracle.bmc.objectstorage.responses.GetBucketResponse;
+import io.micronaut.core.annotation.TypeHint;
 import io.micronaut.runtime.Micronaut;
 
+@TypeHint(
+        value = {
+                CreatePreauthenticatedRequestDetails.class,
+                PutObjectRequest.class,
+                PutObjectRequest.Builder.class,
+                PreauthenticatedRequest.class,
+                PreauthenticatedRequest.Builder.class,
+                GetBucketResponse.class,
+                Bucket.Builder.class
+        },
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+                TypeHint.AccessType.ALL_PUBLIC_METHODS,
+                TypeHint.AccessType.ALL_DECLARED_FIELDS
+        }
+)
 public class Application {
 
     private final FileWatcherService fileWatcherService;
--- src/main/java/codes/recursive/FileWatcherService.java
+++ src/main/java/codes/recursive/FileWatcherService.java
@@ -1,42 +1,32 @@
 package codes.recursive;
 
-import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
-import com.oracle.bmc.http.*;
-import com.oracle.bmc.objectstorage.ObjectStorage;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
-import com.oracle.bmc.objectstorage.model.*;
-import com.oracle.bmc.objectstorage.requests.*;
-import com.oracle.bmc.objectstorage.responses.*;
+import com.oracle.bmc.objectstorage.model.Bucket;
+import com.oracle.bmc.objectstorage.model.CreatePreauthenticatedRequestDetails;
+import com.oracle.bmc.objectstorage.model.PreauthenticatedRequest;
+import com.oracle.bmc.objectstorage.requests.CreatePreauthenticatedRequestRequest;
+import com.oracle.bmc.objectstorage.requests.GetBucketRequest;
+import com.oracle.bmc.objectstorage.requests.PutObjectRequest;
+import com.oracle.bmc.objectstorage.responses.CreatePreauthenticatedRequestResponse;
+import com.oracle.bmc.objectstorage.responses.GetBucketResponse;
+import com.oracle.bmc.objectstorage.responses.PutObjectResponse;
 import io.micronaut.context.annotation.Property;
-import io.micronaut.core.util.CollectionUtils;
 import jakarta.inject.Singleton;
-import nl.altindag.ssl.SSLFactory;
-import nl.altindag.ssl.util.CertificateUtils;
-import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.glassfish.jersey.client.ClientConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManagerFactory;
-import javax.ws.rs.client.ClientBuilder;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.*;
+import java.util.Date;
+import java.util.UUID;
 
 @Singleton
 public class FileWatcherService {
@@ -46,43 +36,27 @@ public class FileWatcherService {
     private final String bucket;
     private final String namespace;
     private final String region;
+    private final Integer parDurationHours;
+    private final Boolean isPrivateBucket;
 
     public FileWatcherService(
             ObjectStorageClient objectStorageClient,
-            @Property(name = "codes.recursive.upload-dir") String uploadDir,
-            @Property(name = "codes.recursive.oci.bucket") String bucket,
-            @Property(name = "codes.recursive.oci.namespace") String namespace,
-            @Property(name = "codes.recursive.oci.region") String region
+            @Property(name = "local-upload-dir") String uploadDir,
+            @Property(name = "oci-bucket") String bucket,
+            @Property(name = "oci-namespace") String namespace,
+            @Property(name = "oci-region") String region,
+            @Property(name = "oci-par-duration-hours") Integer parDurationHours
     ) throws IOException, NoSuchAlgorithmException, CertificateException, KeyStoreException, KeyManagementException {
-        //this.objectStorageClient = objectStorageClient;
+        this.objectStorageClient = objectStorageClient;
         this.uploadDir = uploadDir;
         this.bucket = bucket;
         this.namespace = namespace;
         this.region = region;
+        this.parDurationHours = parDurationHours;
 
-        File crtFile = new File("/Users/trsharp/Library/Preferences/httptoolkit/ca.pem");
-        Certificate certificate = CertificateFactory.getInstance("X.509").generateCertificate(new FileInputStream(crtFile));
-        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-        keyStore.load(null, null);
-        keyStore.setCertificateEntry("server", certificate);
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustManagerFactory.getTrustManagers(), null);
-        ApacheProxyConfig proxyConfig = ApacheProxyConfig.builder().uri("http://localhost:8000").build();
-        ClientConfigDecorator proxyConfigDecorator = new ApacheProxyConfigDecorator(proxyConfig);
-        ApacheConnectorProperties apacheConnectorProperties =
-                ApacheConnectorProperties.builder()
-                        .sslContext(sslContext)
-                        .hostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                        .build();
-        ApacheConfigurator configurator = new ApacheConfigurator.NonBuffering(
-                apacheConnectorProperties,
-                Collections.singletonList(proxyConfigDecorator)
-        );
-        this.objectStorageClient = ObjectStorageClient.builder()
-                .clientConfigurator(configurator)
-                .build(new ConfigFileAuthenticationDetailsProvider("DEFAULT"));
+        // cache bucket visibility
+        GetBucketResponse getBucketResponse = objectStorageClient.getBucket(GetBucketRequest.builder().bucketName(bucket).namespaceName(namespace).build());
+        this.isPrivateBucket = getBucketResponse.getBucket().getPublicAccessType().equals(Bucket.PublicAccessType.NoPublicAccess);
     }
 
     public void watch() throws IOException, InterruptedException {
@@ -100,50 +74,56 @@ public class FileWatcherService {
                 String objectName = event.context().toString();
                 String filePath = uploadDir + "/" + objectName;
                 File changedFile = new File(filePath);
-                String preAuthUrl = "https://objectstorage." + region + ".oraclecloud.com";
+                String contentType = Files.probeContentType(changedFile.toPath());
+                String baseUrl = "https://objectstorage." + region + ".oraclecloud.com";
+                String objectUrl;
 
                 // if this is a modify/create event, upload the object
                 if (!StandardWatchEventKinds.ENTRY_DELETE.equals(event.kind())){
-
+                    // upload the object
                     PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                                     .objectName(event.context().toString())
                                     .namespaceName(namespace)
                                     .bucketName(bucket)
                                     .putObjectBody(new FileInputStream(changedFile))
+                                    .contentType(contentType)
                                     .build();
                     PutObjectResponse putObjectResponse = objectStorageClient.putObject(putObjectRequest);
 
-                    RenameObjectResponse renameObjectResponse = objectStorageClient.renameObject(RenameObjectRequest.builder().bucketName(bucket).namespaceName(namespace).renameObjectDetails(RenameObjectDetails.builder().sourceName(objectName).newName("new.jpg").build()).build());
-                    System.out.println(renameObjectResponse.toString());
-
-                    // get a pre-authenticated request for the uploaded object
-                    CreatePreauthenticatedRequestDetails requestDetails = CreatePreauthenticatedRequestDetails.builder()
-                            .name("PAR_" + UUID.randomUUID().toString())
-                            .bucketListingAction(PreauthenticatedRequest.BucketListingAction.Deny)
-                            .accessType(CreatePreauthenticatedRequestDetails.AccessType.ObjectRead)
-                            .objectName("new.jpg")
-                            .timeExpires(Date.from( LocalDateTime.now().plusHours(24).atZone(ZoneId.systemDefault()).toInstant() ) )
-                            .build();
-                    System.out.println(requestDetails);
-                    CreatePreauthenticatedRequestRequest preAuthenticatedRequest = CreatePreauthenticatedRequestRequest.builder()
-                            .bucketName(bucket)
-                            .namespaceName(namespace)
-                            .createPreauthenticatedRequestDetails(requestDetails)
-                            .build();
-                    System.out.println(preAuthenticatedRequest);
-                    CreatePreauthenticatedRequestResponse preAuthenticatedRequestResponse = objectStorageClient.createPreauthenticatedRequest(preAuthenticatedRequest);
-                    preAuthUrl += preAuthenticatedRequestResponse.getPreauthenticatedRequest().getAccessUri();
+                    if(isPrivateBucket) {
+                        // get a pre-authenticated request for the uploaded object
+                        CreatePreauthenticatedRequestDetails requestDetails = CreatePreauthenticatedRequestDetails.builder()
+                                .name("PAR_" + UUID.randomUUID().toString())
+                                .bucketListingAction(PreauthenticatedRequest.BucketListingAction.Deny)
+                                .accessType(CreatePreauthenticatedRequestDetails.AccessType.ObjectRead)
+                                .objectName(objectName)
+                                .timeExpires(Date.from(LocalDateTime.now().plusHours(parDurationHours).atZone(ZoneId.systemDefault()).toInstant()))
+                                .build();
+                        CreatePreauthenticatedRequestRequest preAuthenticatedRequest = CreatePreauthenticatedRequestRequest.builder()
+                                .bucketName(bucket)
+                                .namespaceName(namespace)
+                                .createPreauthenticatedRequestDetails(requestDetails)
+                                .build();
+                        CreatePreauthenticatedRequestResponse preAuthenticatedRequestResponse = objectStorageClient.createPreauthenticatedRequest(preAuthenticatedRequest);
+                        objectUrl = baseUrl + preAuthenticatedRequestResponse.getPreauthenticatedRequest().getAccessUri();
+                    }
+                    else {
+                        objectUrl = baseUrl +  "/n/" + namespace + "/b/" + bucket + "/o/" + objectName.replaceAll(" ", "%20");
+                    }
 
                     // copy URL to clipboard and pop desktop notification
                     // depends on https://github.com/vjeantet/alerter
                     ProcessBuilder processBuilder = new ProcessBuilder();
+                    String message = "'" + objectName + "' has been uploaded to '" + bucket + "'. URL has been copied to clipboard.";
+                    LOG.info(message);
+                    LOG.info("URL: {}", objectUrl);
                     String[] args = {
-                            "echo " + preAuthUrl + " | pbcopy && ",
+                            "echo " + objectUrl + " | pbcopy && ",
                             "/Users/trsharp/bin/alerter",
                             "-timeout 5",
                             "-title \"Object Uploaded!\"",
                             "-sender hubdocuments.oracle.webcenter.app",
-                            "-message \"" + objectName + " has been uploaded to '" + bucket + "'. URL has been copied to clipboard.\"",
+                            "-message \"" + message + "\"",
                             "-appIcon https://objectstorage.us-phoenix-1.oraclecloud.com/n/toddrsharp/b/object-upload-demo-public/o/cloud.png",
                     };
                     processBuilder.command("bash", "-c", String.join(" ",args));
